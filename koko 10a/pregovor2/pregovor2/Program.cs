001e35ae1f05525fe0690c1ec0a6b34609bbf6a5
Kolev69 <120708761+Kolev69@users.noreply.github.com>
2023-09-25 11:12:03 +0300

    Add files via upload

---
+++ koko 10a/pregovor2/pregovor2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pregovor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             int uchenici = int.Parse(Console.ReadLine());
+            do
+            {
+             
+               Console.WriteLine("Въведди число");
+            }
+               
+            while(x<1 || x => 14 )
+
+                
+            
+
+            
+
+        }
+    }
+}
